48cc56400ebec61706ad6a134bed1f30aa7bbf73
Anderson <anderzon.0077@gmail.com>
2024-10-04 09:25:51 -0500

    Ejercicio_01

--- Condicionales_y_operadores.dart
+++ Condicionales_y_operadores.dart
@@ -1,38 +1,17 @@
 import 'dart:io';
 
 void main() {
-  print('¡Hola! Bienvenido al calculador de años.');
-  print('Escribe tu nombre: ');
-  String? nameInput = stdin.readLineSync();
-  print('Un gusto conocerte $nameInput');
-  print('Escribe tu fecha de nacimiento en formato dd/mm/yyyy: ');
-  String? birthdayInput = stdin.readLineSync();
-  DateTime? dateConverted = _convertToDateTime(birthdayInput!);
-  if (dateConverted == null) {
-    print('La fecha de nacimiento es inválida, nos enviaste: $birthdayInput');
-    return;
+  print("Ingrese el primer número:");
+
+  double? a = double.tryParse(stdin.readLineSync()!);
+  print("Ingrese el segundo número:");
+  double? b = double.tryParse(stdin.readLineSync()!);
+
+  const indeterminado = "Indeterminado";
+  if (a != null && b != null) {
+    print(
+        "Suma: ${a + b}, Resta: ${a - b}, Multiplicación: ${a * b}, División: ${(b != 0 ? a / b : indeterminado)}");
+  } else {
+    print("Por favor, ingrese valores numéricos válidos.");
   }
-
-  var person = Person(
-    name: nameInput,
-    birthday: dateConverted,
-    age: DateTime.now().year - dateConverted.year,
-  );
-
-  print('Tu nombre: ${person.name}');
-  print('Tu fecha de nacimiento: ${person.birthday}');
-  print('¡Tu edad es ${person.age} años!');
-}
-
-DateTime? _convertToDateTime(String date) {
-  List<String> parts = date.split('/');
-  if (parts.length != 3) return null;
-  return DateTime.tryParse('${parts[2]}-${parts[1]}-${parts[0]}');
-}
-
-class Person {
-  final String? name;
-  final DateTime? birthday;
-  final int? age;
-  Person({this.name, this.birthday, this.age});
 }
